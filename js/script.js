a26a65291f0e9145b9684a381871f0a3b685eec7
JoyXu123 <xu.jingyi1@husky.neu.edu>
2021-06-19 22:11:40 -0400

    change to grey color

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.hobbyList').bind("mouseover", function(){
         var color = $(this).css("background-color");
 
-        $(this).css("background", "#380606");
+        $(this).css("background", "#d3d3d3");
 
         $(this).bind("mouseout", function(){
             $(this).css("background", color);
